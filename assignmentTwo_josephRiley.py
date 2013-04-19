d6f4dece21379ae4183872f4bf852bed5e4c3276
fooshnip <riley.jos@gmail.com>
2013-04-19 01:01:59 -0500

    'shortest path assignment complete'

--- assignmentTwo_josephRiley.py
+++ assignmentTwo_josephRiley.py
@@ -6,22 +6,66 @@ Created on Apr 14, 2013
 ## Energy Analytics - Assignment 2 - Shortest Path Algorithm
 
 #first step will be to create a random network
-import pandas as pd
 import numpy as np
 import random as rand
 
-numNodes = rand.randrange(500,1000)
-data = np.array([np.arange(numNodes)]*numNodes)
-df = pd.DataFrame(data, index=range(0,numNodes), columns=range(0,numNodes))
-for i in df:
-    for j in df:
-        df[i][j] = rand.randrange(-1,100)
-#now have data frame with distance matrix
-print df.describe()
-
-#need to sample nodeTo
+#create cost matrix
+numNodes = 11
+cost = np.random.random_integers(low=-1,high=100,size=(numNodes,numNodes))
+np.fill_diagonal(cost,0)
+#print cost
 
 
+#create sampling dictionary with arcs and labels
+sampleNet = {0:{}}
+labelstart=1000000
+print sampleNet
+#0 will be start and 1 will be end
+sampleNet[0] = {'arcs':dict(zip(range(2,4),cost[0,range(2,4)])),'label':0}
+print sampleNet
+sampleNet[1]= {'arcs':{1:0},'label':labelstart}
+print sampleNet
+sample = []
+for i in range(2,numNodes):
+    sample = rand.sample((range(2,i)+range(i+1,numNodes)),numNodes/3) #avoid selecting i in sample
+    unitcost = []
+    for j in sample:
+        unitcost.append(cost[i,j])
+    sampleNet[i] = {'arcs':dict(zip(sample,unitcost)),'label':labelstart}
+for i in range(numNodes-2,numNodes):
+    sample = rand.sample((range(2,i)+range(i+1,numNodes)),numNodes/3-1)
+    sample.append(1)
+    unitcost = []
+    for j in sample:
+        unitcost.append(cost[i,j])
+    sampleNet[i] = {'arcs':dict(zip(sample,unitcost)),'label':labelstart}
 
+print sampleNet
 
-        
+#net={0:dict(arcs={},label=sys.maxint)}
+#{0:{'arcs':{1:121,..},'label':10000}}
+
+s=0
+t=1
+S=[]
+print S
+for i in range(len(sampleNet.keys())):
+    sl = 1000000
+    bn = None
+    for j in range(len(sampleNet.keys())):
+        if not j in S:
+            if sampleNet[j]['label'] < sl:
+                #print sampleNet[j]['label']
+                sl = sampleNet[j]['label']
+                bn = j
+                
+    S.append(bn)
+    if 1 in S: break
+    print S, sampleNet
+    for z in sampleNet[bn]['arcs']:     
+        for k,v in sampleNet[bn]['arcs'].items():
+            sampleNet[k]['label'] = min(sampleNet[k]['label'],(sampleNet[bn]['label']+sampleNet[bn]['arcs'][k]))
+#            print labelofk #cost of arc from bn to k: net[bn][arcs][k]
+
+print sampleNet
+print sampleNet[1]['label']
